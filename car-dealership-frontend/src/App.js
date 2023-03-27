45a65c36c00e3fc924c11f938f73ce9283318d48
Sammy <sammy.liaop@gmail.com>
2023-03-27 15:42:50 -0400

    before a merge

--- car-dealership-frontend/src/App.js
+++ car-dealership-frontend/src/App.js
@@ -1,15 +1,41 @@
 import './App.css';
 import axios from 'axios'
 import {useState, useEffect} from 'react'
+import Car from './components/Car'
+import Edit from './components/Edit'
+import Add from './components/Add'
 
 const App = () => {
+  //Declare cars
+  const[cars, setCars] = useState ([])
+  //Boolean for add statement
+  const[add, setAdd] = useState(false)
 
+  //Get data for cars from the backend
+  const getCars = () => {
+    axios.get('http://localhost:3000/cars').then((response) => {
+      setCars(response.data)
+    })
+  }
 
-  // useEffect(() => {
+  const addCar = () => {
+    setAdd(!add)
+  }
+ 
+  useEffect(() => {
+    getCars()
+  }, [])
 
-  // }, [])
   return(
     <>
+      <h1>Cars</h1>
+      {cars.map((car) => {
+        return(
+          <Car 
+            car = {car}
+          />
+        )
+      })}
     </>
   )
 }
---
+++ car-dealership-frontend/src/components/Add.js
@@ -0,0 +1,27 @@
+// import {useState} from 'react'
+// import axios from 'axios'
+
+// const Add = (props) => {
+//     const [name, setName] = useState()
+//     const [image, setImage] = useState()
+//     const [type, setType] = useState()
+//     const [year, setYear] = useState()
+
+//     //Update data with Form
+
+//     //Send data to Database with submit
+//     const handleAddCar = (event) => {
+//         event.preventDefault()
+//         axios.post('http://localhost:3000/cars').then(() => {
+//             props.
+//         })
+//     }
+//     return (
+//         <>
+//             <h2>New Car for Sale</h2>
+//             <form onSubmit = {handleAddCar}
+//         </>
+//     )
+// }
+
+// export default Add;
