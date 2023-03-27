c198d8cdfa03c4b335be1d65e11e81f4160a46b7
Sammy <sammy.liaop@gmail.com>
2023-03-27 14:37:17 -0400

    react skeleton finished

---
+++ car-dealership-frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+
+const App = () => {
+
+
+  // useEffect(() => {
+
+  // }, [])
+  return(
+    <>
+    </>
+  )
+}
+
+export default App;
